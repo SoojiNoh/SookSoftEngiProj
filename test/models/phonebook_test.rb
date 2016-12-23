4b1d3f8c4b0f92753d3b8858f2b47ef330e5a0de
alchanyeolmae <choco12343@hanmail.net>
2016-12-23 10:19:33 +0000

    testing용

--- test/models/phonebook_test.rb
+++ test/models/phonebook_test.rb
@@ -3,9 +3,10 @@ require 'test_helper'
 class PhonebookTest < ActiveSupport::TestCase
   test "phonebook save" do
      @phonebook = Phonebook.new({
-       name: '석지욘',
+       name: '석지연',
        number: '010-7195-3774'
      })
      assert !phonebook.save, 'Failed to save'
+     assert !phonebook.destroy, 'Failed to destroy'
   end
 end
